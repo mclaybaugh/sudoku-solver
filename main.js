5f6b8b7f13efcfc74a85d833b5efbc3697a93628
Michael Claybaugh <mjc6nr@virginia.edu>
2019-11-04 14:58:21 -0500

    set possible values successfully

--- main.js
+++ main.js
@@ -1,20 +1,121 @@
 document.addEventListener('DOMContentLoaded', () => {
   createBlankCells();
 });
-function update() {
-  console.log('hello');
-}
-function createBlankCells() {
-  var container = document.querySelector('.sudoku');
-  for (var row = 1; row < 10; row++) {
-    for (var col = 1; col < 10; col++) {
-      var cell = document.createElement('input');
-      cell.setAttribute('type', 'number');
-      //cell.setAttribute('max', '9');
-      cell.setAttribute('min', '1');
-      cell.className = `cell ${row} ${col}`;
-      cell.addEventListener('change', update);
-      container.appendChild(cell);
+function getBlock(row, col) {
+  if (col < 4) {
+    if (row < 4) {
+      return 1;
+    } else if (row < 7) {
+      return 4;
+    } else {
+      return 7;
+    }
+  } else if (col < 7) {
+    if (row < 4) {
+      return 2;
+    } else if (row < 7) {
+      return 5;
+    } else {
+      return 8;
+    }
+  } else {
+    if (row < 4) {
+      return 3;
+    } else if (row < 7) {
+      return 6;
+    } else {
+      return 9;
     }
   }
+}
+function getContent(cell) {
+  return cell.querySelector('.cell-content').value;
+}
+function process(arr, vals) {
+  for (let cell of arr) {
+    let val = getContent(cell);
+    if (val) {
+      vals.push(val);
+    }
+  }
+  return vals;
+}
+function getAdjacents(cell) {
+  let values = [];
+  // get nums in row, col, block
+  let row = cell.className.match(/row-[0-9]/)[0];
+  let col = cell.className.match(/col-[0-9]/)[0];
+  let block = cell.className.match(/block-[0-9]/)[0];
+  let rowCells = document.querySelectorAll(`.${row}:not(.${col})`);
+  let colCells = document.querySelectorAll(`.${col}:not(.${row})`);
+  let blockCells = document.querySelectorAll(`.${block}:not(.${row}):not(.${col})`);
+  values = process(colCells, values);
+  values = process(rowCells, values);
+  values = process(blockCells, values);
+
+  return values;
+}
+function update() {
+  let cells = document.querySelectorAll('.cell');
+  for (let cell of cells) {
+    console.log(cell.className);
+    let value = getContent(cell);
+    let adjacents = getAdjacents(cell);
+    if (value) {
+      // remove notes
+      for (let num = 1; num < 10; num++) {
+        cell.classList.remove(`note-${num}`);
+      }
+      if (adjacents.indexOf(value) < 0) {
+        cell.classList.add('cell-success');
+        cell.classList.remove('cell-error');
+      } else {
+        cell.classList.add('cell-error');
+        cell.classList.remove('cell-success');
+      }
+    } else {
+      for (let num = 1; num < 10; num++) {
+        let numClass = `note-${num}`;
+        if (adjacents.indexOf(`${num}`) < 0) {
+          cell.classList.add(numClass);
+        } else {
+          cell.classList.remove(numClass);
+        }
+      }
+    }
+    // set state as error, in-progress, success
+  }
+}
+function createBlankCells() {
+  let container = document.querySelector('.sudoku');
+  for (let row = 1; row < 10; row++) {
+    for (let col = 1; col < 10; col++) {
+      container.appendChild(createCell(row, col));
+    }
+  }
+}
+function createCell(row, col) {
+  let cell = document.createElement('div');
+  let block = getBlock(row, col);
+  cell.className = `cell row-${row} col-${col} block-${block}`;
+
+  let options = document.createElement('div');
+  options.className = 'cell-options';
+  for (let i = 1; i < 10; i++) {
+    let option = document.createElement('div');
+    option.className = `option option-${i}`;
+    option.innerHTML = i;
+    options.appendChild(option);
+  }
+  cell.appendChild(options);
+
+  let content = document.createElement('input');
+  content.className = 'cell-content';
+  content.setAttribute('type', 'number');
+  content.setAttribute('min', '1');
+  content.setAttribute('max', '9');
+  content.addEventListener('change', update);
+  cell.appendChild(content);
+
+  return cell;
 }
